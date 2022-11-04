93554a1553152a918035d24bc3163c578f8f9054
varsha998c <varsha998c@gmail.com>
2022-11-04 17:42:18 +0530

    last update

--- js/Script.js
+++ js/Script.js
@@ -1,3 +1,32 @@
-$(window).ready(function () {
-    $(".single-item").slick();
-});
+// menubar
+function menuFunction() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function myFunction() {
+    var x = document.getElementById("visa");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function closeFunction() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function test() {
+    let a = document.getElementById("visa");
+    let value = a.value;
+    let display = document.getElementById("textresult");
+    display.value = value;
+}
